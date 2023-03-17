f6feb61fc9b508bb89caa28e7a659c3f5dfa0634
Gautam <kumawatgautam305@gmail.com>
2023-03-17 18:59:20 +0530

    added frontend

---
+++ src/components/Signup.js
@@ -0,0 +1,74 @@
+import React, { useState} from "react"; 
+import axios from "axios";
+ 
+
+function Signup() {
+    const [user, setuser] = useState({
+        name : "",
+        email : "",
+        phone : ""
+    });
+
+    let name,value;
+    function handlechange(event) {
+       name = event.target.name;
+       value = event.target.value;
+       setuser({...user,[name]:value});
+    }
+
+   const handleclick = async(event) => {
+       event.preventDefault();
+        const {name , email, phone} = user;
+        if(name && email && phone)
+        {
+            try{
+                await axios.post("http://localhost:5000/register", {
+                    name,email,phone
+                })
+                .then(res => {
+                    console.log(res.data);
+                })
+            }catch(err) {
+                console.log(err);
+            }
+        }
+        else 
+        {
+            alert("Invalid Input");
+        }
+    }
+
+    return (
+        <>
+        <div className="gautam">
+            <nav className="navbar">
+                <div className="navbar-1">
+                    <h3 className="gk1">AirPool.in</h3>
+                </div>
+            </nav>
+            <div className="home">
+                <h1>Sign Up</h1>
+                <form method = "POST" style={{marginTop: "10px", paddingTop: "10px"}}>
+                    <div className = "home-1">
+                        <div style={{padding: "10px, 10px 5px 10px"}}>
+                            <p><b>Name</b></p>
+                            <input type="text" placeholder="Enter Name" value = {user.name} name = "name" onChange = {handlechange} />
+                        </div>
+                        <div style={{padding: "10px, 10px 5px 10px"}}>
+                            <p><b>Email</b></p>
+                            <input type="text" placeholder="Enter Email Address" value = {user.email} name = "email"  onChange = {handlechange} />
+                        </div>
+                        <div style={{padding: "10px, 10px 5px 10px"}}>
+                            <p><b>Contact No.</b></p>
+                            <input type="text" placeholder="Enter Contact No." value = {user.phone} name = "phone"  onChange = {handlechange} />
+                        </div>
+                        <button type="submit" onClick = {handleclick}><b>Register</b></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Signup;  
